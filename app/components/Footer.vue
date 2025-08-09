1a9d09a6e1d489b3442538b05e42692c0198412f
lealewis <lealewis@Leas-Mac-mini.local>
2025-08-09 19:23:45 +0100

    Use NUXT framework

---
+++ app/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <a href="https://github.com/le2310al/le2310al.github.io">Repository</a>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ app/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-ctp-blue">
+    Lea Lewis
+  </h1>
+</template>
+<script setup lang="ts">
+</script>
