cb06eb5b4c161e5540ce2c7aebbf1623148e12b6
lealewis <lealewis@Leas-Mac-mini.local>
2025-08-12 00:45:27 +0100

    Created Index Template

---
+++ app/components/volt/Fieldset.vue
@@ -0,0 +1,64 @@
+<template>
+    <Fieldset
+        unstyled
+        :pt="theme"
+        :ptOptions="{
+            mergeProps: ptViewMerge
+        }"
+    >
+        <template #togglericon="{ collapsed }">
+          <font-awesome-layers v-if="collapsed" :class="theme.toggleIcon">
+            <font-awesome-icon icon="fa-solid fa-square" class="text-ctp-base"/>
+            <font-awesome-icon icon="fa-solid fa-chevron-left" transform="shrink-5 left-4.5"/>
+            <font-awesome-icon icon="fa-solid fa-chevron-right" transform="shrink-5 right-4.5"/>
+          </font-awesome-layers>
+          <font-awesome-icon icon="fa-solid fa-code" v-else :class="theme.toggleIcon" />
+        </template>
+        <template v-for="(_, slotName) in $slots" #[slotName]="slotProps">
+            <slot :name="slotName" v-bind="slotProps ?? {}" />
+        </template>
+    </Fieldset>
+</template>
+
+<script setup lang="ts">
+import MinusIcon from '@primevue/icons/minus';
+import PlusIcon from '@primevue/icons/plus';
+import Fieldset, { type FieldsetPassThroughOptions, type FieldsetProps } from 'primevue/fieldset';
+import { ref } from 'vue';
+import { ptViewMerge } from './utils';
+import {FontAwesomeIcon, FontAwesomeLayers} from "@fortawesome/vue-fontawesome";
+
+interface Props extends /* @vue-ignore */ FieldsetProps {}
+defineProps<Props>();
+
+const theme = ref<FieldsetPassThroughOptions>({
+    root: `border border-surface-200 dark:border-surface-700 rounded-md
+        bg-surface-0 dark:bg-surface-900 text-surface-700 dark:text-surface-0
+        px-[1.125rem] pt-0 pb-[1.125rem]`,
+    legend: `border border-transparent rounded-md px-3 py-2 p-toggleable:p-0
+        transition-colors duration-200`,
+    toggleButton: `select-none overflow-hidden relative group cursor-pointer
+        flex items-center justify-center gap-2
+        px-3 py-2
+        border-none rounded-md
+        bg-surface-0 dark:bg-surface-900
+        hover:bg-surface-100 dark:hover:bg-surface-800
+        text-surface-700 dark:text-surface-0
+        hover:text-surface-800 dark:hover:text-surface-0
+        focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-primary
+        transition-colors duration-200`,
+    toggleIcon: `text-surface-500 dark:text-surface-400 transition-colors duration-200
+        group-hover:text-surface-600 dark:group-hover:text-surface-300`,
+    legendLabel: `font-semibold`,
+    contentContainer: ``,
+    content: `p-0`,
+    transition: {
+        enterFromClass: 'max-h-0',
+        enterActiveClass: 'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
+        enterToClass: 'max-h-[1000px]',
+        leaveFromClass: 'max-h-[1000px]',
+        leaveActiveClass: 'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
+        leaveToClass: 'max-h-0'
+    }
+});
+</script>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-onMounted(() => {
-  window.location.href = 'https://gist.githubusercontent.com/le2310al/682a8f002f386317376e982a5a3cc20c/raw/69ded6678db43cd2fad2464e80abba79338cde3f/contact.vcf'
-})
+// import { onMounted } from 'vue'
+// onMounted(() => {
+//   window.location.href = 'https://gist.githubusercontent.com/le2310al/682a8f002f386317376e982a5a3cc20c/raw/69ded6678db43cd2fad2464e80abba79338cde3f/contact.vcf'
+// })
 </script>
 
 <template>
-  <p>Loading Cyborg Contact</p>
+  <p  class="text-center text-ctp-green mt-8 text-xl">
+    <h2>Loading Cyborg Contact <font-awesome-icon icon="fa-solid fa-spinner" /></h2>
+  </p>
 </template>
 
