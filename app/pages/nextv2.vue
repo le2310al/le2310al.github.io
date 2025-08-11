2995327c7cf4ba914aaf1852fb280bdbc5896699
lealewis <lealewis@Leas-Mac-mini.local>
2025-08-12 00:50:37 +0100

    Re-Named Contact to NExTv2

--- app/pages/nextv2.vue
+++ app/pages/nextv2.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
  import { onMounted } from 'vue'
+ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
  onMounted(() => {
    window.location.href = 'https://gist.githubusercontent.com/le2310al/682a8f002f386317376e982a5a3cc20c/raw/69ded6678db43cd2fad2464e80abba79338cde3f/contact.vcf'
 })
@@ -7,7 +8,7 @@
 
 <template>
   <p  class="text-center text-ctp-green mt-8 text-xl">
-    <h2>Loading Cyborg Contact <font-awesome-icon icon="fa-solid fa-spinner" /></h2>
+    Loading Cyborg Contact <font-awesome-icon icon="fa-solid fa-spinner" />
   </p>
 </template>
 
