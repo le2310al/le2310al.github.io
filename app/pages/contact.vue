5e4ef5b31e0983de3d6229a74ad52ccbfabc920b
lealewis <lealewis@Leas-Mac-mini.local>
2025-08-12 00:46:28 +0100

    Re-Enabled VCF Link

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-// import { onMounted } from 'vue'
-// onMounted(() => {
-//   window.location.href = 'https://gist.githubusercontent.com/le2310al/682a8f002f386317376e982a5a3cc20c/raw/69ded6678db43cd2fad2464e80abba79338cde3f/contact.vcf'
-// })
+ import { onMounted } from 'vue'
+ onMounted(() => {
+   window.location.href = 'https://gist.githubusercontent.com/le2310al/682a8f002f386317376e982a5a3cc20c/raw/69ded6678db43cd2fad2464e80abba79338cde3f/contact.vcf'
+})
 </script>
 
 <template>
