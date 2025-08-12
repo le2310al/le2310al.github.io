db23f078b260d9e43809fed55b149bf65429be28
lealewis <lealewis@Leas-Mac-mini.local>
2025-08-12 01:16:55 +0100

    Humour Update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,8 +10,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
     <Avatar image="images/avatar_too.jpeg" class="m-8 max-w-full h-auto object-contain" size="xlarge" shape="circle" />
     <Fieldset legend="About" toggleable class="ml-20 mr-20">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Born in Germany, raised in the US, educated in the UK and working ... at your company?
       </p>
     </Fieldset>
     <Fieldset class="ml-20 mr-20">
@@ -22,8 +21,25 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
         </div>
       </template>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      </p>
+    </Fieldset>
+    <Fieldset class="ml-20 mr-20">
+      <template #legend>
+        <div class="flex items-center pl-2">
+          <font-awesome-icon icon="fa-solid fa-robot" shape="circle" />
+          <span class="font-bold p-2">NExT v2 Implant</span>
+        </div>
+      </template>
+      <p>
+       I self-installed a <a href="https://dangerousthings.com/product/next-v2/?srsltid=AfmBOopy0mqvxNtu1pJLIUN4bo66JD-
+       2UjyhTnDsBOn9abLYGkc7HwSy" class="text-ctp-blue">subdermal implant</a> containing both a 13.56MHz NFC and 125kHz RFID chip between my left
+        thumb and index finger. </p>
+      <p>My new magic tricks include entering my carpark by encouragingly petting the gate sensor
+        and making others believe I truly did punch my digits into their phone at the speed of <s>high frequency transmission</s>
+        light.
+      </p>
+      <p>If we don't have the pleasure of meeting in person, check out my <a href="/nextv2" class="text-ctp-blue"> .vcf </a>!
       </p>
     </Fieldset>
   </div>
